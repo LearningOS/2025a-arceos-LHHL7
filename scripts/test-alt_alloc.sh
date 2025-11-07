6884c15bd67f22e6ac39433b9a2a48894948092c
LHHL7 <1826716740@qq.com>
2025-11-07 21:08:11 +0800

    test2:alt_alloc retry

--- scripts/test-alt_alloc.sh
+++ scripts/test-alt_alloc.sh
@@ -16,7 +16,9 @@ make disk_img
 
 make A=exercises/alt_alloc/ run | tee $tmp_file
 
-output=$(tail -n1 ./$tmp_file | grep -a "$grep_content")
+# output=$(tail -n1 ./$tmp_file | grep -a "$grep_content")
+# 修改：检查整个文件而不是最后一行
+output=$(grep -a "$grep_content" ./$tmp_file)
 
 rm -rf $tmp_file 
 
